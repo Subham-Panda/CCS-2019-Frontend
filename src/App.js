0a2efa163e1a16311e88633c4f04891f4f0be3ee
Namit Nathwani <namsnath@gmail.com>
2019-10-26 00:31:45 +0530

    refactor: convert App to a class

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import API from './API';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
 
 	doSomething = () => {
 		API.get('/')
-			.then(res => {})
-			.catch(err => {});
+			.then(res => { })
+			.catch(err => { });
 	}
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Dunno. Do some shit. </p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Dunno. Do some shit. </p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
 				</a>
-			</header>
-		</div>
-	);
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
