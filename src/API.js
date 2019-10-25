5a013945d073e4afc84640c5cc92a84c9c0b42a1
Namit Nathwani <namsnath@gmail.com>
2019-10-26 00:18:56 +0530

    feat: define default Axios instance to use across the app

---
+++ src/API.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+var API = axios.create({
+	baseURL: (process.env.NODE_ENV === 'development') ? '' : process.env.REACT_APP_BASE_URL
+});
+
+API.interceptors.response.use(response => {
+	return response;
+}, error => {
+	if (error.response.status === 401) {
+		console.log("Got 401");
+	}
+	return Promise.reject(error);
+});
+
+export default API;
