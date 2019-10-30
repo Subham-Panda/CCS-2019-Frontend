ef8878fdcd0f33ca627235566d6985e4960385f9
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-10-30 15:05:50 +0530

    feat: Images added, position of footer changed to sticky

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 
 import { } from 'react-bootstrap';
 
-import '../css/Home.css'
+import '../css/Home.css';
+
+// Images
+import managementIllustration from '../images/management_01.png';
+import designIllustration from '../images/design_01.png';
+import techIllustration from '../images/tech_01.png';
+import videoIllustration from '../images/video_01.png';
 
 class Home extends React.Component {
     render () {
         return (
-            <div className='home'></div>
+            <div className='home'>
+                <img src={managementIllustration} alt=''/>
+                <img src={designIllustration} alt=''/>
+                <img src={techIllustration} alt=''/>
+                <img src={videoIllustration} alt=''/>
+            </div>
         )
     }
 }
