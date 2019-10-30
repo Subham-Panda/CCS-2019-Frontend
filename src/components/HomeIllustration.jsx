cddff47769eb4feb763966e5260b1f85aec07fab
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-10-30 22:35:10 +0530

    fix: Eslint

--- src/components/HomeIllustration.jsx
+++ src/components/HomeIllustration.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
 import { Col, Button } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom'
 
 import '../css/HomeIllustration.css';
 
 class HomeIllustration extends React.Component {
+    state = {
+        domain: false,
+    }
+
+    handleStartQuiz = (selectedDomain) => {
+        this.setState(() => ({
+            domain: selectedDomain,
+        }));
+    }
+
     render () {
+        console.log(this.state);
+        if (this.state.domain === true) {
+            return <Redirect to='/dashboard' />
+        }
+
         return (
             <Col md='3'
             className='illustrationContainer'
@@ -19,7 +35,8 @@ class HomeIllustration extends React.Component {
                 <Button
                 className='domainButton text-uppercase py-4'
                 size='lg'
-                variant='light'>
+                variant='light'
+                onClick={ () => { this.handleStartQuiz(this.props.domain) } }>
                     {this.props.domain}
                 </Button>
             </Col>
