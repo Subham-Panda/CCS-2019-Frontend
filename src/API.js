8f04ff9659872e517b8623fed81bcb2f56ed3831
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-11-15 23:26:49 +0530

    feat: Quiz page first iteration

--- src/API.js
+++ src/API.js
@@ -4,6 +4,8 @@ var API = axios.create({
 	baseURL: (process.env.NODE_ENV === 'development') ? '' : process.env.REACT_APP_BASE_URL
 });
 
+// TODO: Send tokens with request interceptors
+
 API.interceptors.response.use(response => {
 	return response;
 }, error => {
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
 
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 
 import CSILogo from './CSILogo';
 
 import '../css/Quiz.css';
+
+import illustration from '../images/design_01.png';
 
 class Quiz extends React.Component {
     render() {
         return (
             <Container fluid='true' className='quizContainer'>
                 <CSILogo />
-                <div>Design</div>
-                <div>Question 1)</div>
-
-                <Card className='quizCard'>
-                <Card.Body>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body>
-                </Card>
-                <div>
-                    Answer:
-                    <Card className='quizCard answerCard'>
-                    <Card.Body>
-                        <textarea className='answerText p-3'/>
-                    </Card.Body>
-                    </Card>
+                <div className='d-flex flex-column justify-content-around mainContent'>
+                    <div>
+                        <div className='domainName my-1'>Design</div>
+                        <div className='questionNo'>Question 1)</div>
+                        <Card className='quizCard questionCard my-1'>
+                        <Card.Body>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        </Card>
+                    </div>
+                    <div>
+                        <Row>
+                            <Col md='3'><img src={illustration} alt='Illustration' className='illustration'/></Col>
+                            <Col>
+                                <Card className='quizCard answerCard'>
+                                <Card.Body>
+                                    <textarea placeholder='Your answer here.' className='answerText p-2'/>
+                                </Card.Body>
+                                </Card>
+                            </Col>
+                        </Row>
+                        <Row className='buttonContainer mt-3'>
+                            <Col md='3'></Col>
+                            <Col className='d-flex justify-content-between'>
+                                <Button className='text-uppercase'>Previous</Button>
+                                <div>
+                                    <Button className='text-uppercase mx-4'>Next</Button>
+                                    <Button className='text-uppercase'>Submit</Button>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
             </Container>
         );
