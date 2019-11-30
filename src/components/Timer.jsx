974d4eae31b1b1c2a9d0d2529d55e631fd5c8f1d
Rohan Mukherjee <rohanbayern@gmail.com>
2019-11-30 07:51:35 +0530

    feat: Added time units in boxes

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -27,10 +27,10 @@ class Timer extends React.Component {
         }), 20);
         return (
             <div className="m-auto text-center d-flex justify-content-around timer">
-                <div className="days">{this.state.days}</div>
-                <div className="hours">{this.state.hours}</div>
-                <div className="minutes">{this.state.minutes}</div>
-                <div className="seconds">{this.state.seconds}</div>
+                <div className="days">{this.state.days}<div className="timeUnit">days</div></div>
+                <div className="hours">{this.state.hours}<div className="timeUnit">hours</div></div>
+                <div className="minutes">{this.state.minutes}<div className="timeUnit">minutes</div></div>
+                <div className="seconds">{this.state.seconds}<div className="timeUnit">seconds</div></div>
             </div>
         );
     }
