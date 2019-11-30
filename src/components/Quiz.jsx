9ab496e3c14c771fdcff94084f50c96da77f1972
Rohan Mukherjee <rohanbayern@gmail.com>
2019-11-30 21:42:01 +0530

    feat: Quiz page updated

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-import { Container, Card, Row, Col, Button } from 'react-bootstrap';
+import { Container, Card, Button } from 'react-bootstrap';
 // import $ from 'jquery';
 
-import CSILogo from './CSILogo';
-
+import Timer from './Timer';
 import '../css/Quiz.css';
-
-import illustration from '../images/design_01.png';
 
 class Quiz extends React.Component {
     render() {
         return (
-            <Container fluid='true' className='quizContainer'>
-                <CSILogo />
-                <div className='d-flex flex-column justify-content-around mainContent'>
-                    <div>
-                        <div className='domainName my-1'>Design</div>
-                        <div className='questionNo'>Question 1)</div>
-                        <Card className='quizCard questionCard my-1 py-2'>
+            <Container fluid='true' className='d-flex flex-column justify-content-around quizContainer'>
+                <div>
+                    <div className='d-flex justify-content-between'>
+                        <div className='domainName'>Domain: DESIGN</div>
+                        <div className='timerContainer'><Timer /></div>
+                    </div>
+                    <Card className='quizCard questionCard py-2 my-2'>
+                        <Card.Title className='questionNo pl-3'>Question 1)</Card.Title>
                         <Card.Body className='questionCardBody py-0'>
                             <Card.Text>
                                 Some quick example text to build on the card title and make up the bulk of
@@ -37,32 +35,37 @@ class Quiz extends React.Component {
                                 the card's content.
                             </Card.Text>
                         </Card.Body>
+                    </Card>
+                </div>
+                <div>
+                    <div className='py-2'>
+                        <Card className='quizCard answerCard'>
+                        <Card.Body>
+                            <textarea placeholder='Your answer here.' className='answerText p-2'/>
+                        </Card.Body>
                         </Card>
                     </div>
-                    <div>
-                        <Row className='py-2'>
-                            <Col md='3' className='d-flex flex-column justify-content-center'>
-                                <img src={illustration} alt='Illustration' className='illustration'/>
-                            </Col>
-                            <Col>
-                                <Card className='quizCard answerCard'>
-                                <Card.Body>
-                                    <textarea placeholder='Your answer here.' className='answerText p-2'/>
-                                </Card.Body>
-                                </Card>
-                            </Col>
-                        </Row>
-                        <Row className='buttonContainer mt-1'>
-                            <Col md='3'></Col>
-                            <Col className='d-flex justify-content-between'>
-                                <Button className='text-uppercase'>Previous</Button>
-                                <div>
-                                    <Button className='text-uppercase'>Next</Button>
-                                    <Button className='text-uppercase submitButton ml-4 d-none'>Submit</Button>
-                                </div>
-                            </Col>
-                        </Row>
+                    <div className='buttonContainer mt-1 d-flex justify-content-between'>
+                        <div>
+                        <Button className='text-uppercase'>Previous</Button>
+                        </div>
+                        <div>
+                            <Button className='text-uppercase'>Next</Button>
+                            <Button className='text-uppercase submitButton ml-4'>Submit</Button>
+                        </div>
                     </div>
+                </div>
+                <div className='questionStatusContainer d-flex justify-content-around py-2'>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
+                    <div className='questionStatus text-center'>10</div>
                 </div>
             </Container>
         );
