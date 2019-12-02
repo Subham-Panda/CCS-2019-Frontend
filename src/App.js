e9cd5ba9d75281645312d777a55fb7528d8138a1
Rohan Mukherjee <rohanbayern@gmail.com>
2019-12-02 18:01:01 +0530

    feat: Redirect on login

--- src/App.js
+++ src/App.js
@@ -32,7 +32,10 @@ class App extends React.Component {
 							<Route exact path='/' component={ComingSoon /*Home*/} />
 							<Route path='/quiz' component={Quiz} />
 							<Route path='/register' component={() => {
-								window.location.href="https://accounts.csivit.com/auth/register"
+								window.location.href="https://accounts.csivit.com/oauth/authorize?clientId=294A404E635266556A586E327234753778214125442A472D4B6150645367566B&state=2jen9jfnvjn0nv1e&redirectUrl=https%3A%2F%2Fccs.csivit.com%2Foauth%2Ftoken";
+							}}></Route>
+							<Route path='/oauth/token' component={() => {
+								window.location.href="https://ccs.csivit.com";
 							}}></Route>
           				</Switch>
 					</Row>
