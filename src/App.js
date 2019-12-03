a7d73bae0ab39de57e6468a91a78d0c17801dc70
Rohan Mukherjee <rohanbayern@gmail.com>
2019-12-03 09:07:08 +0530

    fix: Localhost reverted t ccs.csivit

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends React.Component {
 							<Route path='/quiz' component={Quiz} />
 							
 							<Route path='/register' component={() => {
-								window.location.href="https://accounts.csivit.com/oauth/authorize?clientId=294A404E635266556A586E327234753778214125442A472D4B6150645367566B&state=2jen9jfnvjn0nv1e&redirectUrl=http%3A%2F%2Flocalhost:3000%2Foauth%2Ftoken";
+								window.location.href="https://accounts.csivit.com/oauth/authorize?clientId=294A404E635266556A586E327234753778214125442A472D4B6150645367566B&state=2jen9jfnvjn0nv1e&redirectUrl=https%3A%2F%2Fccs.csivit.com%2Foauth%2Ftoken";
 								return null;
 							}} />
 
