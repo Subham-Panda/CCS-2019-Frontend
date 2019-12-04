429e20a290463e676ac2816a5d1a7e16e0ef9405
Rohan Mukherjee <rohanbayern@gmail.com>
2019-12-04 09:13:26 +0530

    feat: Added instructions modal

--- src/components/HomeIllustration.jsx
+++ src/components/HomeIllustration.jsx
@@ -4,23 +4,39 @@ import { Col, Button } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom'
 
 import '../css/HomeIllustration.css';
+import Instructions from './Instructions';
 
 class HomeIllustration extends React.Component {
-    state = {
-        domain: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            domain: false,
+            showModal: false,
+        }
+    }
+
+    setModalShow = (val) => {
+        this.setState(() => ({
+            domain: false,
+            showModal: val,
+        }))
     }
 
     handleStartQuiz = async (selectedDomain) => {
         await this.setState(() => ({
             domain: selectedDomain,
         }));
-        this.props.history.push('/quiz');
+
+        return <Instructions
+            show={true}
+            onHide={() => this.setModalShow(false)}
+        />
     }
 
-    render () {
+    render() {
         return (
             <Col md='3'
-            className='illustrationContainer'
+                className='illustrationContainer'
             >
                 <img
                     src={this.props.imgSrc}
@@ -29,10 +45,10 @@ class HomeIllustration extends React.Component {
                 />
 
                 <Button
-                className='domainButton text-uppercase py-4'
-                size='lg'
-                variant='light'
-                onClick={() => {this.handleStartQuiz(this.props.domain)}}>
+                    className='domainButton text-uppercase py-4'
+                    size='lg'
+                    variant='light'
+                    onClick={() => { this.handleStartQuiz(this.props.domain) }}>
                     {this.props.domain}
                 </Button>
             </Col>
---
+++ src/components/Instructions.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+import '../css/Instructions.css';
+class Instructions extends React.Component {
+    render() {
+        return (
+            <Modal
+                {...this.props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                className='instructionModal'
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Instructions
+                </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <ul>
+                        <li>The first round of CCS is conducted online and is mandatory for inductions.</li>
+                        <li>The first round consists of 4 domains.</li>
+                        <li>Participants can attempt more than one domain.</li>
+                        <li>Each domain will contain 10 questions.</li>
+                        <li>Each domain has been allotted a maximum time of 30 minutes.</li>
+                        <li>Participants are requested to answer the questions honestly in spirit of the event.</li>
+                        <li>Evaluations are up to the discretion of CSI.</li>
+                        <li>Inductions are only open for 1st year students.</li>
+                    </ul>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.props.onHide} className='continueButton'>Continue</Button>
+                    <Button onClick={this.props.onHide} className='instructionCloseButton'>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+}
+
+export default Instructions;
